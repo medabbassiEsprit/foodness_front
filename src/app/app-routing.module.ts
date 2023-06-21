e7acde11aeae5d9bfb2ea81887839c399b80c287
khmayssa <mayssa.khalifa@esprit.tn>
2023-06-21 09:57:04 +0100

    mayssa21062023

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './layout/home/home-component/home-component.component';
+import { LoginComponentComponent } from './layout/login/login-component/login-component.component';
+import { RegisterComponentComponent } from './layout/register/register-component/register-component.component';
+import { HeaderComponentComponent } from './layout/header/header-component/header-component.component';
+import { AdminComponentComponent } from './dashborad/admin/admin-component/admin-component.component';
+
+
+const routes: Routes = [
+  {path:'header',component:HeaderComponentComponent},
+  { path: 'home', component: HomeComponentComponent},
+  {path:'login' , component:LoginComponentComponent},
+  {path:'register',component:RegisterComponentComponent},
+  {path:'admin',component:AdminComponentComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './layout/header/header-component/header-component.component';
+import { FooterComponentComponent } from './layout/footer/footer-component/footer-component.component';
+import { HomeComponentComponent } from './layout/home/home-component/home-component.component';
+import { LoginComponentComponent } from './layout/login/login-component/login-component.component';
+import { RegisterComponentComponent } from './layout/register/register-component/register-component.component';
+import { AdminComponentComponent } from './dashborad/admin/admin-component/admin-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    HomeComponentComponent,
+    LoginComponentComponent,
+    RegisterComponentComponent,
+    AdminComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
